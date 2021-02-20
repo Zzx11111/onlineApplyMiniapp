a3394c14f75b7309c9601554a48194a37bc9b382
zyszzx <1002203386@qq.com>
2021-02-20 23:37:26 +0800

    完成活动列表

--- components/activityCard/activityCard.js
+++ components/activityCard/activityCard.js
@@ -4,20 +4,58 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    activity:{
+      type:Object,
+      value:{},
+      observer:function(newVal){
+        this.setData({
+          activityStartTime:newVal.activityTime
+        })
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    activityName:'打飞机'
+    imageBaseUrl:"http://localhost:3000/",
+    activityStartTime:"",
+    timeEnd:''
+    
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
 
+  },
+  // observers:{
+  //   'activity':function(activity){
+  //     this.setData({
+  //       activityStartTime:activity.activityTime
+  //     })
+  //   }
+  // },
+  lifetimes:{
+    attached(){
+      console.log(Date.now())
+      let nowTime = Date.now()
+      let activityTime = Date.parse(this.data.activityStartTime)
+      //相差天数
+      let differDay = Number(((activityTime - nowTime)/(1000*60*60*24)))
+      if(differDay>1){
+        differDay = Number(differDay.toFixed(0)) + 1 + "天"
+        this.setData({
+          timeEnd:differDay
+        })
+      }else{
+        const differMinute = Number((differDay * 24 * 60).toFixed(0)) + 1 + "分钟"
+        this.setData({
+          timeEnd:differMinute
+        })
+      }
+    }
   }
+  
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,75 +9,20 @@ const {
 } = require('../../utils/util')
 Page({
   data: {
-    //appid:'wxb2bd9a2c08b417bf',
-    //secret:'d725f10afcfa3b6455d955b7cd58caa1',
-    // openID: '',
-    // token: '',
-    // activity: [],
-    // a:false
-    searchValue:""
+    offset:0,
+    limit:10,
+    searchValue:"",
+    activity:[]
   },
   onLoad: async function () {
-    // var method = 'POST'
-    // const res = await requestUrl('http://localhost:3000/v1/activity/getActivity','GET')
-    // console.log(res.data)
-    // this.setData({
-    //   activity:res.data
-    // })
-  },
-  login() {
-
-    var that = this
-    wx.login({
-      timeout: 5000,
-      success(res) {
-        console.log(res)
-        if (res.code) {
-          wx.request({
-            method: 'POST',
-            url: 'http://localhost:3000/v1/user/login',
-            data: {
-              code: res.code
-            },
-            // header:{
-            //   "content-type": "application/x-www-form-urlencoded"
-            // },
-            success(res) {
-              // console.log(res.data.token)
-              that.setData({
-                token: res.data.token
-              })
-            },
-            fail(error) {
-              console.log(error)
-            }
-          })
-        }
-      },
-      fail(err) {
-        console.log(err)
-        console.log('登录失败了')
-      }
+    const {data} = await requestUrl({
+      url:"/v1/activity/getActivity"
     })
-  },
-  getInfo() {
-    var that = this
-    console.log(this.data.token)
-    wx.request({
-      url: 'http://localhost:3000/v1/user/info',
-      header: {
-        token: that.data.token
-      },
-      success(res) {
-        console.log(res)
-      },
-      fail(error) {
-
-      }
+    this.setData({
+      activity:data
     })
   },
   site() {
-
     wx.getLocation({
       // type: 'gcj02',
       // isHighAccuracy:true,
